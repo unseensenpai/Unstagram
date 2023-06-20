5d0a82a43f5e9565dc866253dd120726b85ae10b
Said Gülmez <unseensenpai@hotmail.com>
2023-06-20 21:05:56 +0300

    v0.1.1 release.

--- Unstagram/Program.cs
+++ Unstagram/Program.cs
@@ -6,13 +6,14 @@ ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
 configurationBuilder.AddJsonFile("appsettings.json");
 IConfigurationRoot configuration = configurationBuilder.Build();
 
-AnsiConsole.Write(new FigletText("Welcome to Unstagram!"));
+AnsiConsole.Write(new FigletText("Welcome to \n Unstagram!"));
 Console.WriteLine("Step 1: Copy your instagram 'followers_1.json' and 'follower.json' to this folder.");
 Console.WriteLine("Step 2: Check file names in Appsettings.json");
 Console.WriteLine("Step 3: Press Enter key to start process.");
 do
 {
-    if (Console.ReadKey().Key == ConsoleKey.Enter)
+    ConsoleKeyInfo keyResult = Console.ReadKey();
+    if (keyResult.Key == ConsoleKey.Enter)
     {
         string? followingFileName = configuration.GetValue<string>("FollowingFileName");
         string? followerFileName = configuration.GetValue<string>("FollowerFileName");
@@ -20,15 +21,20 @@ do
         InstagramCalculator.GenerateInformation(followingFileName, followerFileName);
         break;
     }
-    else if (Console.ReadKey().Key == ConsoleKey.Escape)
+    else if (keyResult.Key == ConsoleKey.Escape)
     {
         Environment.Exit(0);
     }
     ;
 } while (true);
-
+AnsiConsole.Write(new FigletText("*-Processing-*"));
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"%{i*10}");
+    Thread.Sleep(100);
+}
 AnsiConsole.Write(new FigletText("Done"));
-Console.WriteLine("Çıkmak için bir tuşa basınız.");
+Console.WriteLine("Press any button to exit.");
 Console.ReadKey();
 Environment.Exit(0);
 
